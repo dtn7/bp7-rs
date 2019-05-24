5d45bae2429012758dd05dce339e62a9838bafdf
Lars Baumgaertner <baumgaertner@cs.tu-darmstadt.de>
2019-05-24 13:21:51 +0200

    cleanup and more decode output for bp7 main tool

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
-use std::env;
 use bp7::helpers::*;
 use bp7::*;
+use std::env;
 
-
-fn usage(filepath : &String) {
+fn usage(filepath: &String) {
     println!("usage {:?} <cmd> [args]", filepath);
     println!("\t decode <hexstring>");
     println!("\t rnd");
-
 }
 
-fn decode(bundle : &String) {
+fn decode(bundle: &String) {
     let buf = unhexify(bundle).unwrap();
     //println!("decode: {:02x?}", &buf);
-    let bndl : Bundle = buf.into();
+    dbg!(serde_cbor::from_slice::<serde_cbor::Value>(
+        &buf).unwrap()
+    );
+    let bndl: Bundle = buf.into();
     dbg!(&bndl);
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-
     if args.len() == 1 {
         usage(&args[0]);
         std::process::exit(1);
     }
-    
+
     let cmd = &args[1];
     match cmd.as_str() {
-        "rnd" => println!("{}\n", hexify(&rnd_bundle(bp7::CreationTimestamp::now()).to_cbor())),
+        "rnd" => println!(
+            "{}\n",
+            hexify(&rnd_bundle(bp7::CreationTimestamp::now()).to_cbor())
+        ),
         "decode" => {
-            if args.len() == 3 {                        
+            if args.len() == 3 {
                 decode(&args[2]);
             } else {
                 usage(&args[0]);
             }
-        },            
-        _ => usage(&args[0])
-    }    
+        }
+        _ => usage(&args[0]),
+    }
 }
