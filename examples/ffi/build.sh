d5cdf59f39166560e7a254f8350d7ed787fe54b8
gh0st <1264131+gh0st42@users.noreply.github.com>
2021-10-08 23:05:24 +0200

    fix(ffi_example): fixed build script of ffi example, adding -lm flag

--- examples/ffi/build.sh
+++ examples/ffi/build.sh
@@ -4,5 +4,5 @@ cargo build --release
 cd ../..
 cbindgen -c cbindgen.toml > target/bp7.h
 cd examples/ffi
-gcc bp7-test.c -I ../../target -L../../target/release -lbp7 -static -lpthread -ldl -O2 -o bp7-test
+gcc bp7-test.c -I ../../target -L../../target/release -lbp7 -static -lpthread -ldl -lm -O2 -o bp7-test
 
