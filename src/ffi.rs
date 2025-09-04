5bab71238860fedd33d63ce7455018bb4b2fd4f6
LeWimbes <12753781+LeWimbes@users.noreply.github.com>
2025-09-04 17:57:50 +0200

    migrate to Rust Edition 2024

--- src/ffi.rs
+++ src/ffi.rs
@@ -31,14 +31,14 @@ pub struct BundleMetaData {
 
 /// A simple test for the bp7 FFI interface.
 /// On success it should always return the number 23.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn bp7_working() -> u8 {
     23
 }
 
 /// Another simple test returning a dynamic buffer with a fixed content.
 /// The returned buffer contains the following bytes: [0x42, 0x43, 0x44, 0x45]
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn bp7_buffer_test() -> *mut Buffer {
     let input = vec![0x42, 0x43, 0x44, 0x45];
 
@@ -50,7 +50,7 @@ pub extern "C" fn bp7_buffer_test() -> *mut Buffer {
 }
 
 /// Generate a random bundle as a raw buffer.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn helper_rnd_bundle() -> *mut Buffer {
     let mut bndl = helpers::rnd_bundle(CreationTimestamp::now());
 
@@ -67,13 +67,13 @@ pub extern "C" fn helper_rnd_bundle() -> *mut Buffer {
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn buffer_free(buf: *mut Buffer) {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn buffer_free(buf: *mut Buffer) { unsafe {
     if buf.is_null() {
         return;
     }
     drop(Box::from_raw(buf));
-}
+}}
 
 /// Try to decode a bundle from a given buffer.
 ///
@@ -83,8 +83,8 @@ pub unsafe extern "C" fn buffer_free(buf: *mut Buffer) {
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn bundle_from_cbor(ptr: *mut Buffer) -> *mut Bundle {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn bundle_from_cbor(ptr: *mut Buffer) -> *mut Bundle { unsafe {
     assert!(!ptr.is_null());
     let buf = &mut *ptr;
     //println!("buf len {}", buf.len);
@@ -100,7 +100,7 @@ pub unsafe extern "C" fn bundle_from_cbor(ptr: *mut Buffer) -> *mut Bundle {
     } else {
         std::ptr::null_mut::<Bundle>()
     }
-}
+}}
 
 /// Encode a given bundle a CBOR byte buffer
 ///
@@ -108,8 +108,8 @@ pub unsafe extern "C" fn bundle_from_cbor(ptr: *mut Buffer) -> *mut Bundle {
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn bundle_to_cbor(bndl: *mut Bundle) -> *mut Buffer {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn bundle_to_cbor(bndl: *mut Bundle) -> *mut Buffer { unsafe {
     assert!(!bndl.is_null());
     let bndl = &mut *bndl;
     let mut buf = bndl.to_cbor().into_boxed_slice();
@@ -117,7 +117,7 @@ pub unsafe extern "C" fn bundle_to_cbor(bndl: *mut Bundle) -> *mut Buffer {
     let len = buf.len() as u32;
     std::mem::forget(buf);
     Box::into_raw(Box::new(Buffer { data, len }))
-}
+}}
 
 /// Create a new bundle with standard configuration and a given payload
 ///
@@ -125,13 +125,13 @@ pub unsafe extern "C" fn bundle_to_cbor(bndl: *mut Bundle) -> *mut Buffer {
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn bundle_new_default(
     src: *const c_char,
     dst: *const c_char,
     lifetime: u64,
     ptr: *mut Buffer,
-) -> *mut Bundle {
+) -> *mut Bundle { unsafe {
     assert!(!src.is_null());
     let c_str_src = CStr::from_ptr(src);
 
@@ -167,20 +167,20 @@ pub unsafe extern "C" fn bundle_new_default(
     b.set_crc(crate::crc::CRC_NO);
     b.sort_canonicals();
     Box::into_raw(Box::new(b))
-}
+}}
 
 /// Frees the memory of a given bundle.
 /// # Safety
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn bundle_free(ptr: *mut Bundle) {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn bundle_free(ptr: *mut Bundle) { unsafe {
     if ptr.is_null() {
         return;
     }
     drop(Box::from_raw(ptr));
-}
+}}
 
 /// Get the metadata from a given bundle.
 ///
@@ -188,8 +188,8 @@ pub unsafe extern "C" fn bundle_free(ptr: *mut Bundle) {
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn bundle_get_metadata(bndl: *mut Bundle) -> *mut BundleMetaData {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn bundle_get_metadata(bndl: *mut Bundle) -> *mut BundleMetaData { unsafe {
     assert!(!bndl.is_null());
     let bndl = &mut *bndl;
     let timestamp = bndl.primary.creation_timestamp.dtntime();
@@ -206,7 +206,7 @@ pub unsafe extern "C" fn bundle_get_metadata(bndl: *mut Bundle) -> *mut BundleMe
         seqno,
         lifetime,
     }))
-}
+}}
 
 /// Frees a BundleMetaData struct.
 ///
@@ -214,8 +214,8 @@ pub unsafe extern "C" fn bundle_get_metadata(bndl: *mut Bundle) -> *mut BundleMe
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn bundle_metadata_free(ptr: *mut BundleMetaData) {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn bundle_metadata_free(ptr: *mut BundleMetaData) { unsafe {
     assert!(!ptr.is_null());
     let meta = &mut *ptr;
     if !meta.src.is_null() {
@@ -225,7 +225,7 @@ pub unsafe extern "C" fn bundle_metadata_free(ptr: *mut BundleMetaData) {
     if !meta.dst.is_null() {
         drop(CString::from_raw(meta.dst));
     }
-}
+}}
 
 /// Check if a given bundle is valid.
 /// This checks the primary block as well as the validity of all canonical bundles.
@@ -234,12 +234,12 @@ pub unsafe extern "C" fn bundle_metadata_free(ptr: *mut BundleMetaData) {
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn bundle_is_valid(bndl: *mut Bundle) -> bool {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn bundle_is_valid(bndl: *mut Bundle) -> bool { unsafe {
     assert!(!bndl.is_null());
     let bndl = &mut *bndl;
     bndl.validate().is_ok()
-}
+}}
 
 /// Get the payload of a given bundle.
 ///
@@ -247,8 +247,8 @@ pub unsafe extern "C" fn bundle_is_valid(bndl: *mut Bundle) -> bool {
 ///
 /// Should only be called from FFI interface.
 /// This function can lead to UB as pointer cannot be validated!
-#[no_mangle]
-pub unsafe extern "C" fn bundle_payload(bndl: *mut Bundle) -> *mut Buffer {
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn bundle_payload(bndl: *mut Bundle) -> *mut Buffer { unsafe {
     if !bndl.is_null() {
         let bndl = &mut *bndl;
         if let Some(payload) = bndl.payload() {
@@ -263,4 +263,4 @@ pub unsafe extern "C" fn bundle_payload(bndl: *mut Bundle) -> *mut Buffer {
         data: std::ptr::null_mut(),
         len: 0,
     }))
-}
+}}
